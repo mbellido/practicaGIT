4bdb2a4deea9a6611350060ee58c0ec76366a1bf
Manolo Bellido <mbellido@enlanet.com>
2019-04-03 21:58:54 +0200

    Actualización: cambio control Notas a NumericUpDown

--- Practica6.1_MBG1819/Form1.Designer.cs
+++ Practica6.1_MBG1819/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Practica6._1_MBG1819
 {
-    partial class Form1
+partial class Form1
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -31,10 +31,11 @@
             this.aluNombre = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.aluNota = new System.Windows.Forms.TextBox();
             this.listaAlumnos = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // aluNombre
@@ -61,13 +62,6 @@
             this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Nota:";
-            // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(375, 42);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(83, 20);
-            this.aluNota.TabIndex = 2;
             // 
             // listaAlumnos
             // 
@@ -97,20 +91,28 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(377, 43);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(120, 20);
+            this.aluNota.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(665, 413);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.listaAlumnos);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.aluNombre);
             this.Name = "Form1";
             this.Text = "Alumnos - MBG1819";
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,10 +123,10 @@
         private System.Windows.Forms.TextBox aluNombre;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox aluNota;
         private System.Windows.Forms.TextBox listaAlumnos;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
